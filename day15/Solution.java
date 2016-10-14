0b57b46ee2877322fbb636a4eb6ff2c67da6b16e
Stephanie Gil <sgil@ualberta.ca>
2016-10-13 19:18:23 -0600

    day 15 task done

---
+++ day15/Solution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+    public static  Node insert(Node head,int data) {
+        Node currNode = head;
+        if (currNode != null) {
+            while (currNode.next != null) {
+                currNode = currNode.next;
+            }
+            currNode.next = new Node(data);
+            return head;
+        }
+        return new Node(data);
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
